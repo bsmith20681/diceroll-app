2126184fd5a035bbff1e676203bbf300c72eb300
bsmith20681 <bsmith20681@gmail.com>
2019-06-17 18:49:17 -0600

    second commit

---
+++ src/Die.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Die.css";
+
+class Die extends Component {
+
+  render() {
+    return (
+      <div>
+        <i class={`Die fas fa-dice-${this.props.face}`}></i>
+      </div>
+    );
+  }
+
+}
+
+export default Die;
